160c10b48a9ba7af6ecf0850d5741a747e2a98d5
ekonaser <152221622+ekonaser@users.noreply.github.com>
2024-12-08 23:33:16 +0100

    Posodobljena verzija skripte

--- obracuni.cpp
+++ obracuni.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
+#include <unordered_map>
+#include <set>
 
 using namespace std;
 
+//###################################FUNKCIJE###################################//
 wstring ocisti_niz(wstring stavek, wstring vzorec, wstring zamenjava = L" ") {
     int pos = 0;
     while ((pos = stavek.find(vzorec, pos)) != wstring::npos) {
@@ -12,6 +16,62 @@ wstring ocisti_niz(wstring stavek, wstring vzorec, wstring zamenjava = L" ") {
     }
     return stavek;
 }
+
+vector<wstring> razdeli(wstring stavek, wchar_t del = L';') {
+    /*Funkcija nam vhodni niz razdeli s pomocjo podanega locila*/
+    wstringstream tok_niza(stavek);
+    vector<wstring> sez_bes;
+    wstring beseda;
+
+    while (getline(tok_niza, beseda, del)) {
+        sez_bes.push_back(beseda);
+    }
+    
+    return sez_bes;
+}
+
+unordered_map<wstring, wstring> ustvariSlovar(const string datoteka) {
+    /*Funkcija nam iz vhodne datoteke ustvari slovar*/
+    unordered_map<wstring, wstring> slo;
+    wifstream dat(datoteka);
+    vector<wstring> vek;
+    wstring vr;
+    while (getline(dat, vr)) {
+        vek = razdeli(vr);
+        slo.insert({vek[0], vek[1]});
+    }
+    return slo;
+}
+
+//####################################RAZRED####################################//
+class Slovar{
+    public:
+        Slovar(const string datoteka) {
+            slo = ustvariSlovar(datoteka);
+        }
+    // globalne spremenljivke
+    unordered_map<wstring, wstring> slo;
+
+    // metode
+        set<wstring> vrednosti() {
+            /*Metoda nam vrne mnozico vrednosti*/
+            set<wstring> sez_vre;
+            for (const auto par: slo) {
+                sez_vre.insert(par.second);
+            }
+            return sez_vre;
+        }
+
+        set<wstring> kljuci() {
+            /*Metoda nam vrne mnozico kljucev*/
+            set<wstring> sez_vre;
+            for (const auto par: slo) {
+                sez_vre.insert(par.first);
+            }
+            return sez_vre;
+        }
+};
+//##############################################################################//
 
 vector<vector<wstring>> izlusci(const string datoteka) {
     /*  Funkcija nam iz .xml datoteke izlusci vrstice in stolpce
@@ -53,15 +113,21 @@ vector<vector<wstring>> izlusci(const string datoteka) {
 
 int main() {
     wofstream dat("obracun.csv");
+    // slovar parov stevilk AWB:MRN
+    unordered_map<wstring, wstring> slo_parov;
     // matrika podatkov
     vector<vector<wstring>> mat_pod;
     mat_pod = izlusci("obracun_tlm.xls");
-    for (vector vec : mat_pod) {
-        wstring vr;
-        for (wstring celica : vec) {
-            vr += celica + L";";
+    Slovar slovar_parov("carm_scan.csv");
+    for (vector vek : mat_pod) {
+        if (slovar_parov.kljuci().count(vek[0]) > 0 || 
+            slovar_parov.vrednosti().count(vek[2]) > 0 ||
+            vek[0] == L"FEDEX" || vek[0] == L"TNT") {
+                for (wstring celica : vek) {
+                    dat << celica << ";";
+                }
+                dat << "\n";
         }
-        dat << vr << '\n';
     }
     dat.close();
     system("pause");
